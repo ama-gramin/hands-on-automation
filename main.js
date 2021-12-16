233fd6f18d46c36ffef8e7dd8985c2a32cfaa36b
ama-gramin <>
2021-12-16 18:47:11 +0900

    3パターン対応

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request == "Action") {
+		inputForm();
+	}
+});
+
+function inputForm() {
+    let columns;
+
+    if (location.href.match(URL_TYPE1)) {
+        columns = COLUMNS_TYPE1;
+    } else if (location.href.match(URL_TYPE2)) {
+        columns = COLUMNS_TYPE2;
+    } else if (location.href.match(URL_TYPE3)) {
+        columns = COLUMNS_TYPE3;
+    }
+
+    if (!columns) {
+        return
+    }
+
+    for (let id in columns) {
+        const element = document.getElementById(id);
+        if (element) {
+            element.value = columns[id];
+        }
+    }
+}
